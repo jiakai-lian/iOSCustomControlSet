6c95201916ee91f6e0ff770fe6e9d6928c9c34a9
jiakailian <jiakai.lian@gmail.com>
2015-07-09 16:21:18 +1000

    add border category

---
+++ Pod/Classes/UIView+Border.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Border.h
+//  Pods
+//
+//  Created by jiakai lian on 9/07/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Border)
+
+@end
---
+++ Pod/Classes/UIView+Border.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Border.m
+//  Pods
+//
+//  Created by jiakai lian on 9/07/2015.
+//
+//
+
+#import "UIView+Border.h"
+
+@implementation UIView (Border)
+
+@end
