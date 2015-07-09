ac98bbdba919b5d7fb8ab7cc703139a608ab0a95
jiakailian <jiakai.lian@gmail.com>
2015-07-09 20:17:47 +1000

    pod update

--- Example/Pods/Target Support Files/Pods-iOSCustomControlSet_Example-iOSCustomControlSet/Pods-iOSCustomControlSet_Example-iOSCustomControlSet-umbrella.h
+++ Example/Pods/Target Support Files/Pods-iOSCustomControlSet_Example-iOSCustomControlSet/Pods-iOSCustomControlSet_Example-iOSCustomControlSet-umbrella.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
+#import "UIView+Border.h"
+#import "UIView+CornerRadius.h"
 
 FOUNDATION_EXPORT double iOSCustomControlSetVersionNumber;
 FOUNDATION_EXPORT const unsigned char iOSCustomControlSetVersionString[];
